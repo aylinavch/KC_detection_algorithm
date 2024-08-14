bf2a52c5021878bf655fd413ca27d8a322b72764
Aylin Vazquez Chenlo <aylina.vch@gmail.com>
2024-08-14 19:42:18 -0300

    Initializing labeling restructuration

---
+++ src/base/labeling.py
@@ -0,0 +1,8 @@
+def run_blind_labeling():
+    print('Running blind labeling')
+
+def run_semi_automatic_labeling():
+    print('Running semiautomatic labeling')
+    
+def run_label_cleaning():
+    print('Running label cleaning')
